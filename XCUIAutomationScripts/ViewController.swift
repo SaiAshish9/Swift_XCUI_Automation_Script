c763d0511c1def1d160b9a1ebd005db875b05374
saiashish9 <saiashish3760@gmail.com>
2022-03-11 14:25:06 +0530

    updated homepage

--- XCUIAutomationScripts/ViewController.swift
+++ XCUIAutomationScripts/ViewController.swift
@@ -11,6 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = UIButton()
+        button.setTitle("XCUI", for: .normal)
+        button.frame = CGRect(x:0,y:0,width:view.frame.width/3,height:50)
+        button.center = view.center
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+        view.backgroundColor = .white
+        view.addSubview(button)
+    }
+    
+    @objc func pressed(sender: UIButton){
+        sender.backgroundColor = sender.backgroundColor == .black ? .white : .black
+        sender.setTitleColor(sender.titleColor(for: .normal) == .black ? .white : .black, for: .normal)
+        view.backgroundColor = view.backgroundColor == .black ? .white : .black
+//        let alert = UIAlertController(title: "XCUI", message: "Tests were passed", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//        self.present(alert,animated: true,completion: nil)
     }
 
 }
