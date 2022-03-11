bfc0d3faa5f0bf378d46c2ef14414af7d8a2fb11
saiashish9 <saiashish3760@gmail.com>
2022-03-11 13:42:26 +0530

    iniitial commit

---
+++ XCUIAutomationScripts/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  XCUIAutomationScripts
+//
+//  Created by Sai Ashish Darapureddy on 11/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
